004d056d7f2937dc755f928887977f32f4d0c2cf
Mythyoung <miaoyangzhen@qq.com>
2017-04-10 21:20:42 +0800

    添加模板示例。

--- cms-web/src/main/java/org/itachi/cms/action/HelloAction.java
+++ cms-web/src/main/java/org/itachi/cms/action/HelloAction.java
@@ -1,5 +1,7 @@
 package org.itachi.cms.action;
 
+import org.glassfish.jersey.server.mvc.Template;
+import org.glassfish.jersey.server.mvc.Viewable;
 import org.itachi.cms.dto.UserDTO;
 import org.itachi.cms.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * Created by itachi on 2017/3/18.
@@ -25,5 +28,12 @@ public class HelloAction extends BaseAction {
     @Produces(APPLICATION_JSON_UTF8)
     public UserDTO hello() throws Exception {
         return userRepository.getUserByW3Id("z00314494");
+    }
+
+    @GET
+    @Path("/index")
+    @Produces(MediaType.TEXT_HTML)
+    public Viewable index() throws Exception {
+        return new Viewable("/index");
     }
 }

f863bdef395cdda8ed90e846d8276e0682da2aa2
liaoyongchao <605086124@qq.com>
2017-05-06 18:07:07 +0800

    用户群组管理界面

--- cms-boot/src/main/java/org/itachi/cms/service/UserGroupService.java
+++ cms-boot/src/main/java/org/itachi/cms/service/UserGroupService.java
@@ -2,8 +2,10 @@ package org.itachi.cms.service;
 
 import org.itachi.cms.dto.AdmuserDTO;
 import org.itachi.cms.dto.RoleTreeDTO;
+import org.itachi.cms.exception.ServiceException;
 import org.itachi.cms.repository.AdmUserGroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
@@ -11,12 +13,13 @@ import java.util.Map;
 /**
  * Created by liaoyongchao on 2017/5/6.
  */
+@Component
 public class UserGroupService {
 
     @Autowired
     private AdmUserGroupRepository admUserGroupRepository;
 
-    public Map<String, Object> findAdmUserGroup(Map<String, Object> map) throws Exception {
+    public Map<String, Object> findAdmUserGroup(Map<String, Object> map) throws ServiceException {
         return admUserGroupRepository.findAdmUserGroup(map);
     }
 }

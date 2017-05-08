79a575454654b44ba491a2ccaf8c21ad2d4b057e
itachizhu <iorikyoshi@163.com>
2017-05-08 13:34:32 +0800

    添加完整的参数异常处理机制。 对应的参考为HelloController中的valid方法。

--- cms-boot/src/main/java/org/itachi/cms/controller/HelloController.java
+++ cms-boot/src/main/java/org/itachi/cms/controller/HelloController.java
@@ -2,9 +2,13 @@ package org.itachi.cms.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.constraints.Size;
 
 /**
  * Created by itachi on 2017/4/23.
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 @RequestMapping("/test")
+@Validated
 public class HelloController {
 
     @RequestMapping(value = "/myerror", method = RequestMethod.GET)
@@ -26,5 +31,11 @@ public class HelloController {
     public String index(Model model) throws Exception {
         model.addAttribute("name", "itachi");
         return "index";
+    }
+
+    @RequestMapping(value = "/valid", method = RequestMethod.GET)
+    @ResponseBody
+    public String valid(@Size(min = 2, max = 20, message = "{id.size}") @RequestParam String id) throws Exception {
+        return id;
     }
 }
--- cms-boot/src/main/java/org/itachi/cms/handler/GlobalExceptionHandler.java
+++ cms-boot/src/main/java/org/itachi/cms/handler/GlobalExceptionHandler.java
@@ -1,19 +1,38 @@
 package org.itachi.cms.handler;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.el.ExpressionFactoryImpl;
+import org.hibernate.validator.internal.engine.MessageInterpolatorContext;
+import org.hibernate.validator.internal.engine.messageinterpolation.InterpolationTerm;
+import org.hibernate.validator.internal.engine.messageinterpolation.InterpolationTermType;
+import org.hibernate.validator.internal.engine.messageinterpolation.parser.MessageDescriptorFormatException;
+import org.hibernate.validator.internal.engine.messageinterpolation.parser.Token;
+import org.hibernate.validator.internal.engine.messageinterpolation.parser.TokenCollector;
+import org.hibernate.validator.internal.engine.messageinterpolation.parser.TokenIterator;
+import org.hibernate.validator.spi.resourceloading.ResourceBundleLocator;
+import org.itachi.cms.util.LocaleUtil;
 import org.itachi.cms.util.ThrowableUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.MessageInterpolator;
+import javax.validation.ValidationException;
+import java.util.*;
 
 /**
  * Created by itachi on 2017/4/23.
@@ -29,6 +48,13 @@ public class GlobalExceptionHandler {
 
     private static final String CODE = "code";
     private static final String MESSAGE = "message";
+    private static final String ERRORS = "errors";
+    private static final String FIELD = "field";
+
+    @Autowired
+    private ResourceBundleLocator resourceBundleLocator;
+
+    private HttpServletRequest request;
 
     @ExceptionHandler(value = Throwable.class)
     public ResponseEntity handleException(Throwable throwable, HttpServletRequest request) {
@@ -36,6 +62,7 @@ public class GlobalExceptionHandler {
         result.put(CODE, 999);
         HttpStatus status = HttpStatus.SERVICE_UNAVAILABLE;
         try {
+            this.request = request;
             status = handleThrowable(result, throwable);
         } catch (Exception e) {
             LOGGER.error("=====ExceptionMapperProvider exception:", e);
@@ -45,9 +72,232 @@ public class GlobalExceptionHandler {
     }
 
     private HttpStatus handleThrowable(Map<String, Object> result, Throwable throwable) throws Exception {
+        if (throwable instanceof ConstraintViolationException) {
+            return handleValidException(result, (ConstraintViolationException)throwable);
+        }
+        if (throwable instanceof MethodArgumentNotValidException) {
+            return handleValidException(result, (MethodArgumentNotValidException)throwable);
+        }
+        if (throwable instanceof MissingServletRequestParameterException) {
+            return handleValidException(result, (MissingServletRequestParameterException)throwable);
+        }
+        if (throwable instanceof MissingPathVariableException) {
+            return handleValidException(result, (MissingPathVariableException)throwable);
+        }
         if (throwable != null && throwable.getCause() != null && throwable.getCause() != throwable) {
             return ThrowableUtil.getInstance().handleException(result, throwable.getCause());
         }
         return ThrowableUtil.getInstance().handleException(result, throwable);
+    }
+
+    private HttpStatus handleValidException(Map<String, Object> result, ConstraintViolationException exception) throws Exception {
+        result.put(CODE, 104);
+        result.put(MESSAGE, "Parameter Error!");
+        Set<ConstraintViolation<?>> messages = exception.getConstraintViolations();
+        if (messages != null && !messages.isEmpty()) {
+            Locale locale = LocaleUtil.getLocale(request);
+            List<Map<String, String>> list = new ArrayList<>();
+            for (ConstraintViolation<?> message : messages) {
+                Map<String, String> map = new HashMap<>();
+                map.put(FIELD, StringUtils.substringAfterLast(message.getPropertyPath().toString(), "."));
+                if (message.getMessageTemplate() != null && message.getMessageTemplate().length() > 0) {
+                    MessageInterpolatorContext context = new MessageInterpolatorContext(message.getConstraintDescriptor(), message.getInvalidValue(), message.getRootBeanClass(), new HashMap<>(0));
+                    map.put(MESSAGE, interpolate(message.getMessageTemplate(), context, locale));
+                } else {
+                    map.put(MESSAGE, message.getMessage());
+                }
+                list.add(map);
+            }
+            result.put(ERRORS, list);
+        }
+
+        return HttpStatus.BAD_REQUEST;
+    }
+
+    private HttpStatus handleValidException(Map<String, Object> result, MissingPathVariableException exception) throws Exception {
+        result.put(CODE, 104);
+        result.put(MESSAGE, "Parameter Error!");
+        List<Map<String, Object>> list = new ArrayList<>();
+        Map<String, Object> map = new HashMap<>();
+        map.put(FIELD, exception.getVariableName());
+        map.put(MESSAGE, exception.getMessage());
+        return HttpStatus.BAD_REQUEST;
+    }
+
+    private HttpStatus handleValidException(Map<String, Object> result, MissingServletRequestParameterException exception) throws Exception {
+        result.put(CODE, 104);
+        result.put(MESSAGE, "Parameter Error!");
+        List<Map<String, Object>> list = new ArrayList<>();
+        Map<String, Object> map = new HashMap<>();
+        map.put(FIELD, exception.getParameterName());
+        map.put(MESSAGE, exception.getMessage());
+        return HttpStatus.BAD_REQUEST;
+    }
+
+    private HttpStatus handleValidException(Map<String, Object> result, MethodArgumentNotValidException exception) throws Exception {
+        result.put(CODE, 104);
+        result.put(MESSAGE, "Parameter Error!");
+        List<Map<String, Object>> list = new ArrayList<>();
+        for (ObjectError error : exception.getBindingResult().getAllErrors()) {
+            Map<String, Object> map = new HashMap<>();
+            map.put(FIELD, exception.getBindingResult().getTarget());
+            map.put(MESSAGE, error.getDefaultMessage());
+            list.add(map);
+        }
+        return HttpStatus.BAD_REQUEST;
+    }
+
+    private String interpolate(String message, MessageInterpolator.Context context, Locale locale) {
+        String interpolatedMessage = message;
+        try {
+            interpolatedMessage = interpolateMessage(message, context, locale);
+        } catch (ValidationException e) {
+            LOGGER.warn(e.getMessage());
+        }
+        return interpolatedMessage;
+    }
+
+    /**
+     * Runs the message interpolation according to algorithm specified in the Bean Validation specification.
+     * <br/>
+     * Note:
+     * <br/>
+     * Look-ups in user bundles is recursive whereas look-ups in default bundle are not!
+     *
+     * @param message the message to interpolate
+     * @param context the context for this interpolation
+     * @param locale  the {@code Locale} to use for the resource bundle.
+     * @return the interpolated message.
+     */
+    private String interpolateMessage(String message, MessageInterpolator.Context context, Locale locale)
+            throws MessageDescriptorFormatException {
+        // LocalizedMessage localisedMessage = new LocalizedMessage(message, locale);
+        String resolvedMessage = null;
+
+        // if the message is not already in the cache we have to run step 1-3 of the message resolution
+        if (resolvedMessage == null) {
+            // ResourceBundleLocator resourceBundleLocator = null;
+
+            ResourceBundle userResourceBundle = resourceBundleLocator
+                    .getResourceBundle(locale);
+            ResourceBundle defaultResourceBundle = resourceBundleLocator
+                    .getResourceBundle(locale);
+
+            String userBundleResolvedMessage;
+            resolvedMessage = message;
+            boolean evaluatedDefaultBundleOnce = false;
+            do {
+                // search the user bundle recursive (step1)
+                userBundleResolvedMessage = interpolateBundleMessage(
+                        resolvedMessage, userResourceBundle, locale, true
+                );
+
+                // exit condition - we have at least tried to validate against the default bundle and there was no
+                // further replacements
+                if (evaluatedDefaultBundleOnce
+                        && !hasReplacementTakenPlace(userBundleResolvedMessage, resolvedMessage)) {
+                    break;
+                }
+
+                // search the default bundle non recursive (step2)
+                resolvedMessage = interpolateBundleMessage(
+                        userBundleResolvedMessage,
+                        defaultResourceBundle,
+                        locale,
+                        false
+                );
+                evaluatedDefaultBundleOnce = true;
+            } while (true);
+        }
+
+        // resolve parameter expressions (step 4)
+        List<Token> tokens = null;
+        if (tokens == null) {
+            TokenCollector tokenCollector = new TokenCollector(resolvedMessage, InterpolationTermType.PARAMETER);
+            tokens = tokenCollector.getTokenList();
+        }
+        resolvedMessage = interpolateExpression(
+                new TokenIterator(tokens),
+                context,
+                locale
+        );
+
+        // resolve EL expressions (step 5)
+        tokens = null;
+        if (tokens == null) {
+            TokenCollector tokenCollector = new TokenCollector(resolvedMessage, InterpolationTermType.EL);
+            tokens = tokenCollector.getTokenList();
+        }
+        resolvedMessage = interpolateExpression(
+                new TokenIterator(tokens),
+                context,
+                locale
+        );
+
+        // last but not least we have to take care of escaped literals
+        resolvedMessage = replaceEscapedLiterals(resolvedMessage);
+
+        return resolvedMessage;
+    }
+
+    private String replaceEscapedLiterals(String resolvedMessage) {
+        resolvedMessage = resolvedMessage.replace("\\{", "{");
+        resolvedMessage = resolvedMessage.replace("\\}", "}");
+        resolvedMessage = resolvedMessage.replace("\\\\", "\\");
+        resolvedMessage = resolvedMessage.replace("\\$", "$");
+        return resolvedMessage;
+    }
+
+    private boolean hasReplacementTakenPlace(String origMessage, String newMessage) {
+        return !origMessage.equals(newMessage);
+    }
+
+    private String interpolateBundleMessage(String message, ResourceBundle bundle, Locale locale, boolean recursive)
+            throws MessageDescriptorFormatException {
+        TokenCollector tokenCollector = new TokenCollector(message, InterpolationTermType.PARAMETER);
+        TokenIterator tokenIterator = new TokenIterator(tokenCollector.getTokenList());
+        while (tokenIterator.hasMoreInterpolationTerms()) {
+            String term = tokenIterator.nextInterpolationTerm();
+            String resolvedParameterValue = resolveParameter(
+                    term, bundle, locale, recursive
+            );
+            tokenIterator.replaceCurrentInterpolationTerm(resolvedParameterValue);
+        }
+        return tokenIterator.getInterpolatedMessage();
+    }
+
+    private String interpolateExpression(TokenIterator tokenIterator, MessageInterpolator.Context context, Locale locale)
+            throws MessageDescriptorFormatException {
+        while (tokenIterator.hasMoreInterpolationTerms()) {
+            String term = tokenIterator.nextInterpolationTerm();
+
+            InterpolationTerm expression = new InterpolationTerm(term, locale, new ExpressionFactoryImpl());
+            String resolvedExpression = expression.interpolate(context);
+            tokenIterator.replaceCurrentInterpolationTerm(resolvedExpression);
+        }
+        return tokenIterator.getInterpolatedMessage();
+    }
+
+    private String resolveParameter(String parameterName, ResourceBundle bundle, Locale locale, boolean recursive)
+            throws MessageDescriptorFormatException {
+        String parameterValue;
+        try {
+            if (bundle != null) {
+                parameterValue = bundle.getString(removeCurlyBraces(parameterName));
+                if (recursive) {
+                    parameterValue = interpolateBundleMessage(parameterValue, bundle, locale, recursive);
+                }
+            } else {
+                parameterValue = parameterName;
+            }
+        } catch (MissingResourceException e) {
+            // return parameter itself
+            parameterValue = parameterName;
+        }
+        return parameterValue;
+    }
+
+    private String removeCurlyBraces(String parameter) {
+        return parameter.substring(1, parameter.length() - 1);
     }
 }

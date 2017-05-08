d30e3fb43bf798891fc45881cb277619e7f043f8
Mythyoung <miaoyangzhen@qq.com>
2017-05-08 18:13:35 +0800

    權限修改 以及首頁

--- cms-boot/src/main/java/org/itachi/cms/controller/HelloController.java
+++ cms-boot/src/main/java/org/itachi/cms/controller/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 /**
